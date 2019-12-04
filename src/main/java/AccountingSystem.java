cdd28df6e457e7b9611fff5aef6b301e88c83150
Mariam Melhem <mary.melhem3@gmail.com>
2019-12-04 14:29:57 +0100

    Implemented createPerson and added persons list to AccountingSystem

--- src/main/java/AccountingSystem.java
+++ src/main/java/AccountingSystem.java
@@ -1,7 +1,18 @@
 import src.main.java.BankName;
 import src.main.java.Bank;
 
+import java.util.ArrayList;
+
 public class AccountingSystem {
+    private ArrayList<Person> persons;
+
+    public AccountingSystem() {
+        this.persons = new ArrayList<>();
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
 
     public void createBank(String s) {
         Bank b = new Bank(s);
@@ -9,6 +20,7 @@ public class AccountingSystem {
 
     public void createPerson(String fn, String ln, int age) {
         Person person = new Person(fn, ln, age);
+        persons.add(person);
     }
 
     public void createAccount(double saldo, Person p, Bank b) {
--- src/test/java/AccoutingSystemTest.java
+++ src/test/java/AccoutingSystemTest.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AccoutingSystemTest {
@@ -14,6 +12,14 @@ public class AccoutingSystemTest {
 
     @Test
     public void createPersonTest() {
+        AccountingSystem accountingSystem = new AccountingSystem();
+
+        accountingSystem.createPerson("John", "Doe", 55);
+
+        Assertions.assertEquals(1, accountingSystem.getPersons().size());
+        Assertions.assertEquals("John", accountingSystem.getPersons().get(0).getFirstName());
+        Assertions.assertEquals("Doe", accountingSystem.getPersons().get(0).getLastName());
+        Assertions.assertEquals(55, accountingSystem.getPersons().get(0).getAge());
     }
 
     @Test
