c98df0155879d55e679a2396bcb062315e80b8af
Halimove01 <halim.dakir@gmail.com>
2019-12-05 17:50:23 +0100

    add methoder pin controller that link all fxml pages and add fxml pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,24 +5,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
+
 public class App extends Application {
-    AppController controller;
+
+    private static Scene scene;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
-        Parent root = loader.load();
-        controller = loader.getController();
-        primaryStage.setTitle("Bank System");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("home"));
+        stage.setScene(scene);
+        stage.show();
     }
 
-
-    public static void main(String[] args){
-        launch(args);
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
     }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
 
 }
--- src/main/java/AppController.java
+++ src/main/java/AppController.java
@@ -1,2 +1,43 @@
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class AppController {
+    @FXML
+    private void createPerson() throws IOException {
+        App.setRoot("person");
+    }
+
+    @FXML
+    private void createBankName() throws IOException {
+        App.setRoot("bank");
+    }
+
+    @FXML
+    private void createAccount() throws IOException {
+        App.setRoot("account");
+    }
+
+    @FXML
+    private void backToHome() throws IOException {
+        App.setRoot("home");
+    }
+    @FXML
+    private void goToNyKund() throws IOException {
+        App.setRoot("nykund");
+    }
+
+    @FXML
+    private void goToRedanKund() throws IOException {
+        App.setRoot("redankund");
+    }
+    @FXML
+    private void goToWithdraw() throws IOException {
+        App.setRoot("withdraw");
+    }
+
+    @FXML
+    private void goToDeposit() throws IOException {
+        App.setRoot("deposit");
+    }
 }
