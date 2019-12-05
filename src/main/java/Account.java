8a10fb6cc16b3326b997a92337649fc974b35d61
Christoffer Clausen <christoffer.clausen@iths.se>
2019-12-05 09:40:44 +0100

    Added motehods and fields to Account class

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,36 @@
+import java.util.UUID;
+
 public class Account {
+
+    private UUID id;
+    private double amount;
+    private Person person;
+    private Bank bank;
+
+    public Account(double a, Person p, Bank b) {
+        this.amount = a;
+        this.person = p;
+        this.bank = b;
+    }
+
+    private void assignId() {
+        id = UUID.randomUUID();
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
 }
