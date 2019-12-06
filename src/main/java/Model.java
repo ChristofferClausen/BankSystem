a704d7ae9e98aa196edb1c7647bda427b5ad3cec
Halimove01 <halim.dakir@gmail.com>
2019-12-06 16:51:36 +0100

    add TODO

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
-    //TODO Set Model Halim will fixe it
+    //TODO Set Model. Halim will fixe it
     List <BankName> accountList = new ArrayList<>();
     List<Person> personList = new ArrayList<>();
     List<BankName> bankNameList = new ArrayList<>();
